faba9f4eb688c35e818ee14b743b47e07ce7c279
Aliyan Ali <darkpikayt1@gmail.com>
2025-02-19 19:28:00 +0500

    app.jsx

---
+++ vite-pro2/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { useState } from 'react';
+
+function App(){
+  const [count, setcount]=useState(0);
+  return(
+    <>
+    
+    <h1>counter: {count}</h1>
+    <button onClick={()=>setcount(count+5)}>Add 5</button>
+    <button onClick={()=>setcount(count-5)} disabled={count===0}>remove 5</button>
+    <button onClick={()=>setcount(0)}>Reset</button>
+    </>
+  )
+}
+
+export default App
+
