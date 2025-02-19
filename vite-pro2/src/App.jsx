10c967d46ef5a419a994ead2d2146ed89d6d8bdf
Aliyan Ali <darkpikayt1@gmail.com>
2025-02-19 20:02:00 +0500

    props

--- vite-pro2/src/App.jsx
+++ vite-pro2/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Hello from './hello';
 import { useState } from 'react';
 
 function App(){
   const [count, setcount]=useState(0);
   return(
     <>
-    
+    <Hello username="ali"/>
     <h1>counter: {count}</h1>
     <button onClick={()=>setcount(count+5)}>Add 5</button>
     <button onClick={()=>setcount(count-5)} disabled={count===0}>remove 5</button>
---
+++ vite-pro2/src/hello.jsx
@@ -0,0 +1,11 @@
+
+
+function Hello({username}){
+    return(
+        <>
+        <h1>hey my name is: {username}</h1>
+        </>
+    )
+}
+
+export default Hello
