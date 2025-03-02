3ae2e9061f49c6949ee143d7727cb383c07d06c0
Aliyan Ali <darkpikayt1@gmail.com>
2025-03-02 10:16:21 +0500

    call api using loader

--- practice/src/main.jsx
+++ practice/src/main.jsx
@@ -1,48 +1,62 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import  {RouterProvider, createBrowserRouter} from  'react-router-dom';
+import  {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from  'react-router-dom';
 import Layout from './Layout';
 import About from './components/About/About';
 import  Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
 import User from './components/User/User';
 import Github from './components/github/Github';
-
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element: <Layout />,
-    children:[
-      {
-        path:'/About',
-        element:<About />
-      },
+import { githubInfoLoader } from './components/github/Github';
+// const router=createBrowserRouter([
+//   {
+//     path:'/',
+//     element: <Layout />,
+//     children:[
+//       {
+//         path:'/About',
+//         element:<About />
+//       },
       
      
-      {
-        path: '/Home',
-        element:<Home />
+//       {
+//         path: '/Home',
+//         element:<Home />
 
-      },
-      {
-        path:'/Contact',
-        element:<Contact />
-      },
-      {
+//       },
+//       {
+//         path:'/Contact',
+//         element:<Contact />
+//       },
+//       {
         
-        path:'Home/:Name',
-        element:<User />
-      },
-      {
+//         path:'Home/:Name',
+//         element:<User />
+//       },
+//       {
         
-        path:'/github',
-        element:<Github/>
-      }
+//         path:'/github',
+//         element:<Github/>
+//       }
 
-    ]
-  }
-])
+//     ]
+//   }
+// ])
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='/About'  element={<About/>} />
+      <Route path='/Home'  element={<Home/>} />
+      <Route path='/Contact'  element={<Contact/>} />
+      <Route path='/Home/:Name' element={<User/>} />
+      <Route
+       loader={githubInfoLoader}
+       path='/Github'  element={<Github/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
