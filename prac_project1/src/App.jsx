cfc3020aad2858cb38658502d6e58e29ad31beb2
Aliyan Ali <darkpikayt1@gmail.com>
2025-03-07 19:27:00 +0500

    app jsx

---
+++ prac_project1/src/App.jsx
@@ -0,0 +1,17 @@
+import Input from "./components/Input"
+import Todos from "./components/Todos"
+
+function App() {
+ 
+
+  return (
+    <>
+    
+     <h1 className="flex item-center justify-center text-2xl font-mono">hey its Todo web app</h1>
+    <Input/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
