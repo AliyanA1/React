c7d0f9d767532401352ee93d138d4171c9790653
Aliyan Ali <darkpikayt1@gmail.com>
2025-03-11 10:22:09 +0500

    Hero jsx

--- Ai_summerizer/src/components/Hero.jsx
+++ Ai_summerizer/src/components/Hero.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import {logo} from '../assets'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="w-full flex  justify-center items-center flex-col">
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+       <img src={logo} alt='logo' className='w-28 object-contain' />
+
+       <button className='black_btn' type="button" 
+       onClick={()=> window.open('https://github.com/AliyanA1/React/tree/main/Ai_summerizer')}
+       >Github</button>
+      </nav>
+
+      <h1 className='head_text'> 
+        Summerize Articals With <br className='max-md:hidden'/>
+       <span className='orange_gradient'>Open-Ai Gpt-4</span> 
+      </h1>
+
+      <h2 className='desc'>
+       Simplify you reading with summize, an
+       open source artical summerizer transform 
+       lenghty artical into clear and concise summaries
+      </h2>
+    </div>
   )
 }
 
