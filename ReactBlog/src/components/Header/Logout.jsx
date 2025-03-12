6e44932a4dac5b3ceef30ff5d9bd7dc059d78b0c
Aliyan Ali <darkpikayt1@gmail.com>
2025-03-12 13:16:48 +0500

    Logout jsx

---
+++ ReactBlog/src/components/Header/Logout.jsx
@@ -0,0 +1,20 @@
+import {useDispatch} from 'react-redux'
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/auth'
+
+const Logout = () => {
+    const dispatch=useDispatch();
+
+    const logoutHandler=()=>{
+        authService.logOut().then(
+            dispatch(logout)
+        )
+    }
+  return (
+    <button className='bg-blue-500 text-white'
+    onClick={logoutHandler}
+    >Logout</button>
+  )
+}
+
+export default Logout
