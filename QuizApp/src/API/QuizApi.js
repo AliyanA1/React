667ec9859531d6d033a0d83e8ab97bf9e9470d02
Aliyan Ali <darkpikayt1@gmail.com>
2025-03-23 12:57:33 +0500

    quizapi js

---
+++ QuizApp/src/API/QuizApi.js
@@ -0,0 +1,33 @@
+export const quiz=[
+    {
+        q1:'what the main things in development',
+        options:['work hard', 'consistency', 'smart work', 'All'  ],
+        Ans: 'All'
+    },
+
+    {
+        q1:'what you feel you get the things you want ',
+        options:['Happy', 'Allhumdulilah', 'Normal', 'something more'  ],
+        Ans:'Allhumdulilah'
+    },
+
+    {
+        q1:'what is the things make man successful',
+        options:['work hard', 'consistency', 'smart work', 'ego'  ],
+        Ans:'work hard'
+    },
+
+    {
+        q1:'whats the main enemy of success',
+        options:['procastination', 'layziness', 'Scrolling', 'fear'  ],
+        Ans:'fear'
+    },
+
+    {
+        q1:'what if you dont win/success',
+        options:['accept lose', 'try agian', 'try until win', 'Nothing'  ],
+        Ans:'try until win'
+    },
+
+   
+]
